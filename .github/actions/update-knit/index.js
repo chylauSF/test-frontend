bfa6678fe21f4b3a0626d21c2541cbdd92e4ac1a
Christine Chois <christine.chois@stitchfix.com>
2022-06-01 15:52:51 -0700

    Adding comments

--- .github/actions/update-knit/index.js
+++ .github/actions/update-knit/index.js
@@ -1,9 +1,24 @@
+// @actions/github
+// @actions/exec
+// @actions/core
+
 const runDate = Date.now();
 async function run() {
   try {
     console.log('======================================');
     console.log('          Knit Updater!               ');
     console.log('======================================');
+
+    // Get latest Release from Knit
+    // console.log the Release from Knit
+
+    // pull from main and git checkout to a new branch with the name of Latest knit release
+
+
+    // Run NPM update knit version
+    // git add and commit
+    // create a PR based on the commit
+    // 
 
     console.log(runDate)
 
@@ -16,3 +31,7 @@ run();
 
 
 
+// octokit.rest.repos.getLatestRelease({
+//   owner,
+//   repo,
+// });
