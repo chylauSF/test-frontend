3141442608825e505647193bd59dd02683073ba7
Christine Chois <christine.chois@stitchfix.com>
2022-06-01 15:55:47 -0700

    Building dist index to remove node modules

---
+++ .github/actions/update-knit/dist/index.js
@@ -0,0 +1,47 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// @actions/github
+// @actions/exec
+// @actions/core
+// npm install --save @actions/github @actions/exec
+const runDate = Date.now();
+async function run() {
+  try {
+    console.log('======================================');
+    console.log('          Knit Updater!               ');
+    console.log('======================================');
+
+    // Get latest Release from Knit
+    // console.log the Release from Knit
+
+    // pull from main and git checkout to a new branch with the name of Latest knit release
+
+
+    // Run NPM update knit version
+    // git add and commit
+    // create a PR based on the commit
+    // 
+
+    console.log(runDate)
+
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+}
+
+run();
+
+
+
+// octokit.rest.repos.getLatestRelease({
+//   owner,
+//   repo,
+// });
+module.exports = __webpack_exports__;
+/******/ })()
+;
