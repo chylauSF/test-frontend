02a1448b6c8dddfd51359e370777058d5fc56f7a
Christine Chois <christine.chois@stitchfix.com>
2022-06-01 17:15:52 -0700

    Adding exec

--- .github/actions/update-knit/index.js
+++ .github/actions/update-knit/index.js
@@ -1,7 +1,7 @@
 
-// @actions/core
 const core = require("@actions/core");
 const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 async function run() {
   const runDate = Date.now();
